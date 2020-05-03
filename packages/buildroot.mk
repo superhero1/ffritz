96cd1eec5294d6a2fa3d981d6461ff815a16a3b5
Felix Schmidt <fesc2000@gmail.com>
2020-05-03 19:21:42 +0200

    Cleanup buildroot targets if patches/modifications change.

--- packages/buildroot.mk
+++ packages/buildroot.mk
@@ -9,7 +9,6 @@ endif
 ifeq ($(PATCHES),)
 PATCHES=$(wildcard *.patch)
 endif
-PATCHST=$(PATCHES:%=.%.applied)
 
 ifeq ($(BUILDROOT_AVM),)
 URL	= $(shell cat $(PKGTOP)/url-buildroot$(BR_VERSION))
@@ -29,61 +28,77 @@ ifneq ($(PARALLEL),)
 BR_FLAGS += -j$(PARALLEL)
 endif
 
-all:	.build.stamp patches .config_all.stamp
-	make -C build $(BR_FLAGS) $(GCCFLAGS)
-	if [ "$(BUILDROOT_TARGETS)" != "" ]; then make -C build $(BR_FLAGS) $(BUILDROOT_TARGETS) -j2; fi
-
-base:	.build.stamp patches
-	make -C build $(BR_FLAGS) $(GCCFLAGS) -j2
-	make -C build $(BR_FLAGS) $(BASE_TARGETS) -j2
-
-patches: $(PATCHST) .pkg.applied
-
-.pkg.applied:
-	for d in `cd package; ls -d *`; do \
-	    mkdir -p build/package/$$d; \
-	    cp -ar --backup package/$$d/* build/package/$$d; \
-	    done
-	cp mod/*_defconfig build/configs
-ifneq ($(PARALLEL),)
-	echo 'BR2_PER_PACKAGE_DIRECTORIES=y' >> build/configs/ffritz_defconfig
-#	echo 'BR2_PER_PACKAGE_DIRECTORIES=y' >> build/configs/ffritz_sdk_defconfig
-else
-	sed -ie 's/BR2_PER_PACKAGE_DIRECTORIES=y/BR2_PER_PACKAGE_DIRECTORIES=n/' build/configs/ffritz_defconfig
-#	sed -ie 's/BR2_PER_PACKAGE_DIRECTORIES=y/BR2_PER_PACKAGE_DIRECTORIES=n/' build/configs/ffritz_sdk_defconfig
+DEFCONF_FILE = $(wildcard user_defconfig)
+ifeq ($(DEFCONF_FILE),)
+DEFCONF_FILE = mod/ffritz_defconfig
 endif
+DEFCONF = $(shell basename $(DEFCONF_FILE))
+DEFCONF_TGT = $(BUILDDIR)/configs/$(DEFCONF)
+
+PACKAGE_MODS = $(shell find package -type f)
+PACKAGE_MOD_TGT = $(PACKAGE_MODS:%=build/%)
+
+all:	$(BUILDDIR) $(PACKAGE_MOD_TGT) $(DEFCONF_TGT)
+ifeq ($(DEFCONF),user_defconfig)
+	@touch mod/ffritz_defconfig
+endif
+	@if [ -r .clean ]; then make -C build `sort -u .clean`; fi
+	@rm -f .clean
+	@make -C build $(BR_FLAGS)
+	@if [ "$(BUILDROOT_TARGETS)" != "" ]; \
+		then make -C build $(BR_FLAGS) $(BUILDROOT_TARGETS); fi
+
+$(DEFCONF_TGT): $(DEFCONF_FILE)
+	@cp $(DEFCONF_FILE) $(DEFCONF_TGT)
+ifneq ($(PARALLEL),)
+	@echo 'BR2_PER_PACKAGE_DIRECTORIES=y' >> $(DEFCONF_TGT)
+else
+	@sed -ie 's/BR2_PER_PACKAGE_DIRECTORIES=y/BR2_PER_PACKAGE_DIRECTORIES=n/' $(DEFCONF_TGT)
+endif
+	echo Using configuration: $(DEFCONF)
+	@make -C $(BUILDDIR) $(DEFCONF)
+
+$(PACKAGE_MOD_TGT): $(PACKAGE_MODS)
+	@mkdir -p `dirname $@`
+	@if ! diff $(@:build/%=%) $@ 2>&1 > /dev/null; then \
+		install $(@:build/%=%) $@; \
+		echo $@ | \
+		 sed -e 's@build/package/\([^/]*\).*@\1-dirclean@' >> .clean;\
+	fi
+
+base:	$(BUILDDIR) $(DEFCONF_TGT) $(PACKAGE_MOD_TGT)
+	make -C build $(BR_FLAGS) $(TOOLCHAIN)
+ifneq ($(BASE_TARGETS),)
+	make -C build $(BR_FLAGS) $(BASE_TARGETS)
+endif
+
 	touch $@
 
-.config_all.stamp:
-	rm -f .config_*.stamp
-	@if [ -f user_defconfig ]; then install -v user_defconfig build/configs/user_defconfig; make -C build user_defconfig; else make -C build ffritz_defconfig; fi
-	touch $@
-
-$(PATCHST): $(@:.%.applied=%)
-	@echo Applying $(@:.%.applied=%)
-	@cd build; patch -p1 < ../$(@:.%.applied=%)
-	@touch $@
-
-userconfig: .build.stamp
-	@if [ ! -f build/configs/user_defconfig ]; then cp build/configs/ffritz_defconfig build/configs/user_defconfig; fi
-	@make -C build user_defconfig 
-	@make -C build menuconfig
-	@make -C build savedefconfig
-	@cp build/configs/user_defconfig user_defconfig;
-	@echo
-	@echo +++ new config saved to `pwd`/user_defconfig
-	@echo 
+userconfig: $(BUILDDIR) $(PACKAGE_MOD_TGT)
+	if [ ! -f build/configs/user_defconfig ]; then cp build/configs/ffritz_defconfig build/configs/user_defconfig; fi
+	make -C build user_defconfig 
+	make -C build menuconfig
+	make -C build savedefconfig
+	cp build/configs/user_defconfig user_defconfig;
+	echo
+	echo +++ new config saved to `pwd`/user_defconfig
+	echo 
 
 $(FILE):
-	@cd $(DLDIR); wget $(URL)
+	cd $(DLDIR); wget $(URL)
 
-.build.stamp:	$(FILE) 
-	mkdir -p build
-	cd build; tar xf $(FILE) --strip-components=1
-	touch .build.stamp
+$(BUILDDIR): $(FILE) $(PATCHES)
+	@mkdir -p $(BUILDDIR)
+	@cd $(BUILDDIR); tar xf $(FILE) --strip-components=1
+	@cd build; for p in $(PATCHES); do \
+		patch -p1 < ../$$p |\
+		grep 'patching file package' |\
+		sed -e 's@.*package/\([^/]*\).*@\1-dirclean@' >> ../.clean;\
+	done
+	@touch $(BUILDDIR)
 
 clean:
-	rm -rf build .*.stamp .*.applied sdk-buildroot
+	rm -rf build .clean .*.stamp .*.applied sdk-buildroot
 
 sdk:	sdk-buildroot/relocate-sdk.sh
 
